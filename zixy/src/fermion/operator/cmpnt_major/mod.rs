db0bd72e2b43c884af83c0ec0670072a323f6467
Georgia Christopoulou <georgia.christopoulou@quantinuum.com>
2026-06-26 13:17:27 +0100

    linting

--- zixy/src/fermion/operator/cmpnt_major/mod.rs
+++ zixy/src/fermion/operator/cmpnt_major/mod.rs
@@ -1,11 +1,8 @@
 //!Fermion operator module where the storage buffer is component (string) major.
 
 pub mod lincomb;
-<<<<<<< HEAD
-pub mod raw_term_set;
-=======
 pub mod num_ops;
->>>>>>> fermion-integration
+pub mod raw_term_set;
 pub mod term;
 pub mod term_set;
 pub mod terms;

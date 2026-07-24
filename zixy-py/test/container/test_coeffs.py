c8a170adc28eccbef60da063677fdd09da1d912f
Georgia Christopoulou <georgia.christopoulou@quantinuum.com>
2026-07-24 11:48:59 +0100

    correct test

--- zixy-py/test/container/test_coeffs.py
+++ zixy-py/test/container/test_coeffs.py
@@ -621,11 +621,11 @@ def test_real_vec():
 def test_parse_deprecated():
     assert RealCoeffs.from_str("1.0, 2.0") == RealCoeffs.from_sequence([1.0, 2.0])
     with pytest.deprecated_call():
-        assert RealCoeffs.from_str("1.0, 2.0") == RealCoeffs.from_sequence([1.0, 2.0])
+        assert RealCoeffs.parse("1.0, 2.0") == RealCoeffs.from_sequence([1.0, 2.0])
     with pytest.raises(NotImplementedError):
         SymbolicCoeffs.from_str("x, y")
     with pytest.deprecated_call(), pytest.raises(NotImplementedError):
-        SymbolicCoeffs.from_str("x, y")
+        SymbolicCoeffs.parse("x, y")
 
 
 def test_complex_vec():
